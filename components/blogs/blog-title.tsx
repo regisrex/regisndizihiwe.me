c7bb61b7cf49ae68c5f0c11f728f7bd38390fc55
regisrex <ndizihiweregis06@gmail.com>
2023-10-11 22:04:02 +0200

    fix(web): removed non working next-seo

--- components/blogs/blog-title.tsx
+++ components/blogs/blog-title.tsx
@@ -1,4 +1,3 @@
-import { NextSeo } from "next-seo"
 export default function BlogTitle({
     id,
     releaseDate,
@@ -14,25 +13,6 @@ export default function BlogTitle({
 }) {
     return (
         <>
-            <NextSeo
-                title={title}
-                description={description}
-                canonical="https://www.canonical.ie/"
-                openGraph={{
-                    url: `https://regisndizihiwe.me/blog/${id}`,
-                    title: title,
-                    description: description,
-                    images: [
-                        { url: '/og-image.png', }
-                    ],
-                    siteName: 'Regis NDIZIHIWE | Blog',
-                }}
-                twitter={{
-                    handle: '@regissrex',
-                    site: 'www.regisndizihiwe.me',
-                    cardType: 'image/png',
-                }}
-            />
             <div className="flex flex-col gap-2 mb-6 py-4 border-b border-neutral-800">
                 <p className="opacity-60 font-sans italic">Released on: {releaseDate}</p>
                 <h3 className='text-2xl font-bold text-white'>{title}</h3>
