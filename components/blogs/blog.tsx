28ecbba544c48bea4ca7bbec517637e25876f074
regisrex <ndizihiweregis06@gmail.com>
2023-10-26 15:26:53 +0200

    style(web): fonts

--- components/blogs/blog.tsx
+++ components/blogs/blog.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Blog(props: BlogI) {
     return (
         <Link href={`/blog/${props.id}`} className="my-2">
-            <div className="hover:bg-white/[0.04] rounded-lg duration-150 p-3">
-                <h4 className="font-semibold text-lg">{props.title}</h4>
+            <div>
+                <h4 className="font-semibold text-[18px] hover:underline">{props.title}</h4>
                 {/* <h4 className="opacity-60">{props.description}</h4> */}
                 <span className="opacity-60 italic font-normal">Released on: {props.releaseDate}</span>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import RexImage from "@/assets/me.jpg"
-import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-6 items-center  justify-between py-24 ">
-      <div className="flex gap-6 items-center">
-        <div className="p-1 rounded-full bg-white bg-opacity-10 border border-neutral-800 h-fit">
-          <Image src={RexImage.src} width={200} height={200} alt="Rex image" className="rounded-full w-[140px] overflow-clip  object-cover" />
-        </div>
-        <div className="flex flex-col justify-between gap-2">
-          <h1 className="text-2xl font-bold bg-gradient-to-br text-transparent from-blue-600 to-red-500 w-fit bg-clip-text">R&eacute;gis NDIZIHIWE</h1>
-          <p className="opacity-60"> Software programmer, student at Rwanda Coding Academy, member of the Hack Club</p>
+    <main className="flex flex-col gap-6   justify-between py-24 ">
+      <div className="flex-col gap-2 items-center">
+        {/* <div className="p-1 rounded-md bg-white bg-opacity-10 border border-neutral-800 w-fit h-fit">
+          <Image src={RexImage.src} width={120} height={120} alt="Rex image" className="rounded-md w-[140px] overflow-clip  object-cover" />
+        </div> */}
+        <div className="flex flex-col justify-between  gap-2">
+          <h1 className="text-xl font-bold bg-gradient-to-br">R&eacute;gis NDIZIHIWE</h1>
+          <p className="opacity-60 text-sm">Programmer</p>
         </div>
       </div>
       <div>
         <div className=" flex flex-col gap-4 ">
           <div className="flex flex-col gap-4">
-            <p>Sup nerd! Welcome to my spot.</p>
-            <p>I write code, I code in Typescript, Go and I&apos;m planning to add Rust, but I actually enjoy coding in any strongly typed language apart from Java, seriously, Java!</p>
+            <p>Howdy!</p>
+            <p>I write code in Typescript and Go </p>
             <p>In my spare time, i build random things that can be useful in some occassions or just scroll into my github feed, that&apos;s where I get some fresh air! </p>
             <p>For a link up, ping me on <a href="mailto:hey@regisndizihiwe.me" className="underline-offset-4 hover:decoration-neutral-200 underline decoration-neutral-600">hey@regisndizihiwe.me</a></p>
 
