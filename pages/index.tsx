4a26efa75937b6070b0355c701cf5a702a9cc910
regisrex <ndizihiweregis06@gmail.com>
2023-11-02 19:51:33 +0200

    fix(web): content

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <div className=" flex flex-col gap-4 ">
           <div className="flex flex-col gap-4">
             <p className="leading-7">
-              Hello! My name is Regis NDIZIHIWE. I&apos;m a programmer who loves coding in TypeScript and Go.
+              Hello! My name is Regis NDIZIHIWE. I&apos;m a programmer👨‍💻 and student📚. I love building applications and tools by writing code.
               I&apos;m also a blogger and a member of Hack Club. Professionally, I&apos;m a web and mobile developer. In my spare time, I tinker on useful hacky project ideas including CLIs, bots, packages, and much more. You can find some on my GitHub.
               Feel free to reach out to me for a chat or collaboration at <a href="mailto:hey@regisndizihiwe.me" className="underline-offset-4 hover:decoration-neutral-200 text-blue-400 decoration-neutral-600">hey@regisndizihiwe.me</a>.
               You can also follow me on <a href="https://github.com/regisrex" className="text-blue-400 decoration-neutral-600  hover:decoration-neutral-200 underline-offset-4">Github</a> and <a href="https://twitter.com/regissrex" className="text-blue-400 decoration-neutral-600 hover:decoration-neutral-200 underline-offset-4">Twitter</a> to see what I&apos;m up to!
